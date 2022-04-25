c0dc41792ce9497447af738b1cfe627687d7778b
chloe <chloe@alliance.hk>
2022-04-25 19:27:44 +0800

    update i18n for jp and cn

--- lib/src/i18n/strings_jp.dart
+++ lib/src/i18n/strings_jp.dart
@@ -60,7 +60,19 @@ class _StringsJp extends _StringsI18n {
 
   @override
   List<String>? getMonthsShort() {
-    // TODO: implement getMonthsShort
-    return null;
+    return [
+      "一月",
+      "二月",
+      "三月",
+      "四月",
+      "五月",
+      "六月",
+      "七月",
+      "八月",
+      "九月",
+      "十月",
+      "十一月",
+      "十二月"
+    ];
   }
 }
